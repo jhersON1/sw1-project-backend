e25ca8b3a74a7e20f7d0185541edad426a83c079
Carlos Andres Vargas Muyuro <vargascarlos2170@gmail.com>
2023-11-18 20:41:33 +0000

    update profileImage, imagesIA

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { ImagesModule } from './images/images.module';
 import { BillingModule } from './billing/billing.module';
 
 @Module({
@@ -21,8 +20,7 @@ import { BillingModule } from './billing/billing.module';
       synchronize: true,
     }),
     AuthModule,
-    ImagesModule,
-    BillingModule,
+    BillingModule
   ],
   controllers: [AppController],
   providers: [AppService],
